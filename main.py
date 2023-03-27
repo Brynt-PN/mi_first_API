c4be1182709bdafc4b22db3a93b6d0bf09b87e76
Brayant_PN <70989902bpn@gmail.com>
2023-03-26 21:41:42 -0500

    Añadimos una ruta para filtrar por ID de Pelicula

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ app.version = '0.0.1' # Con esto colocamos la version a la documentación de la 
 def message():
     return HTMLResponse('<h1>HOLA MUNDO</h1>')
 
+# Definimos una lista de diccionarios que contiene información sobre películas
 movies = [
     {
         'id': 1,
@@ -24,9 +25,26 @@ movies = [
         'year': '2009',
         'rating': 7.8,
         'category': 'Acción'    
+    },
+    {
+        'id': 2,
+        'title': 'Duro de Matar',
+        'overview': "Un policia de NY llega al Nacatomi Plaza para ver a su ex esposa e hijos por navidad y termina en un secuestro terrorista ...",
+        'year': '1988',
+        'rating': 9.4,
+        'category': 'Acción'    
     } 
 ]
 
+# Definimos una ruta para la API que devuelve toda la lista de películas
 @app.get('/movies',tags=['MOVIES'])
 def get_movies():
     return movies
+
+# Definimos una ruta para la API que recibe un ID de película y devuelve la película correspondiente
+@app.get('/movies/{id}', tags=['MOVIES'])
+def get_movie(id: int): # Indicamos que el ID debe ser un INT (Entero)
+    for item in movies:
+        if item['id'] == id:# Verificamos si el valor de la clave 'id' coincide con el valor del parámetro 'id' proporcionado
+            return item # Retornamos el ITEM que seria el dicciónario correspondiente
+    return 'ID invalido'
