455af1e00be715995c11c1ac4aa99f023a1d71f3
Brayant_PN <70989902bpn@gmail.com>
2023-03-28 01:20:39 -0500

    Agregamos FILTER y cplocamos un marcador en Platzy para seguir mañana

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #CODIGO COMENTADO DE INTRODUCCIÓN EN LA RAMA INT
 from fastapi import FastAPI, Body 
 from fastapi.responses import HTMLResponse 
-from pydantic import BaseModel # Importamos BASEMODEL para poder hacer esquemas (Crear clases que ereden caracteristicas de un MODELO BASE)
+from pydantic import BaseModel, Field # Importamos BASEMODEL para poder hacer esquemas (Crear clases que ereden caracteristicas de un MODELO BASE)
 from typing import Optional # Importamos para poder indicar que hay valores opcionales
 
 app = FastAPI() 
@@ -11,10 +11,10 @@ app.version = '0.0.1'
 # Creamos una clase para los Objetos MOVIE, que ereda de BASEMODEL (Modelo Base)
 class Movie(BaseModel):# Luego indicamos las caracteristicas de esta clase (Asi ahorramos codigo mas adelante en POST Y DELET)
     id: Optional[int] = None # Con OPTIONAL indicamos que el ID es un valor que puede ser Opcional, con '[int]' indicamos su valor entero y con '= None' decimos que su valor predeterminado es NULO
-    title: str
-    overview: str
-    year: int
-    rating: float
+    title: str = Field(default= 'Mi Pelicula', min_length= 5, max_length= 15)
+    overview: str = Field(default= 'Descripción de la Pelicula', min_length= 15, max_length= 50)
+    year: int = Field(default= '2022', le= 2022)
+    rating: float = Field(default= 'Ratio',le= 9.9)
     category: str
 
 @app.get('/', tags=['HOME']) 
@@ -57,15 +57,16 @@ def get_movies_by_category(category: str):
     return [item for item in movies if item['category'] == category]
 
 @app.post('/movies', tags=['MOVIES'])
-def create_movie(movie: Movie): # Declaramos que queremos un objeto 'movie' y que es de la clase 'Movie'
-    movies.append(movie) # Aqui indicamos que añada 'movie' directamente en lugar de pasar todos los parametros, ya que al ser un objeto los tiene como caracteristicas.
+def create_movie(movie: Movie): # De la misma forma que indicamos el tipo de parametro podemos pasarle una clase como tipo de parametro de tal forma que 'movie: Movie' indica que el parametro movie es de la clase Movie. (Es como instaurarlo)
+
+    movies.append(movie) # Agregamos a la lista 'movies' el objeto 'movie', aqui solo pasamos ese parametro y nos ahorramos colocar todos las caracteristicas.
     return movies
 
 @app.put('/movies/{id}', tags=['MOVIES'])
 def update_movies(id: int, movie: Movie): # Nuevamente instauramos el objeto 'movie' de la clase 'Movie', dejamos el parametro ID por que ese si lo necesitamos como parametro de filtro
     for item in movies: 
         if item['id'] == id: 
-            item['title'] = movie.title # Con esta sintaxis 'movie.title' indicamos que tome el valor de la caracteristica TITLE del objeto 'movie'
+            item['title'] = movie.title # Con esta sintaxis 'movie.title' accedemos a la caraccteristica 'title' del objeto 'movie'.
             item['overview'] = movie.overview
             item['year'] = movie.year
             item['rating'] = movie.rating
